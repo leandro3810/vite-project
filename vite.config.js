0696b5da253f896294795138756e4e29fd99d569
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2025-05-08 20:03:10 -0300

    vite-projeto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,26 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-projet/',
+  base: '/vite-projet/projeto-vite + react/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [react()],
 })
