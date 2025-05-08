b6371676397adfdf310fc865c0fae91d7a53f556
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2025-05-08 19:37:59 -0300

    vite-projet

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-react/',
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  },
+  base: '/vite-projet/',
   plugins: [react()],
 })
